ebea6b4ca18aa0e5f5b11e26d4384599c4ab4235
flixcrls <carlos.felix200084@gmail.com>
2020-08-15 15:33:34 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -6,5 +6,8 @@
 <body>
     <h1>hello world!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae ut ex quisquam magni aliquid assumenda. Ab aperiam optio, modi iure ad tenetur numquam. Praesentium perspiciatis voluptas, omnis a delectus consequuntur!</p>
+    <ul>
+        <li>Felix carlos</li>
+    </ul>
 </body>
 </html>
